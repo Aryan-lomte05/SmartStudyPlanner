6e026c76432071ea40796ac0143926b1bfb97813
Aryan-lomte05 <aryan.lomte@somaiya.edu>
2025-04-21 00:35:17 +0530

    Source code set

---
+++ SmartStudyPlanner-main/src/ui/CalendarPanel.java
@@ -0,0 +1,15 @@
+package ui;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class CalendarPanel extends JPanel {
+    public CalendarPanel()
+    {
+        add(new JLabel("Calendar Panel-display calendar here!!!!"));
+
+    }
+
+
+
+}
---
+++ SmartStudyPlanner-main/src/ui/SchedulerPanel.java
@@ -0,0 +1,15 @@
+package ui;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class SchedulerPanel extends JPanel {
+    public SchedulerPanel()
+    {
+        add(new JLabel("Scheduler Panel-plan schedule here!!!!"));
+
+    }
+
+
+
+}
